aa866586a81014b644a97736b7cee00f2b0a446a
Rodrigo Martins <rdmartins.dev@protonmail.com>
2025-04-19 01:46:04 -0300

    Essential Neovim Setup 1.0

---
+++ lua/configs/treesitter.lua
@@ -0,0 +1,36 @@
+local options = {
+    ensure_installed = {
+        "bash",
+        "c",
+        "cmake",
+        "cpp",
+        "fish",
+        "go",
+        "gomod",
+        "gosum",
+        "gotmpl",
+        "gowork",
+        "lua",
+        "luadoc",
+        "make",
+        "markdown",
+        "odin",
+        "printf",
+        "python",
+        "rust",
+        "asm",
+        "toml",
+        "vim",
+        "vimdoc",
+        "yaml",
+    },
+
+    highlight = {
+        enable = true,
+        use_languagetree = true,
+    },
+
+    indent = { enable = true },
+}
+
+require("nvim-treesitter.configs").setup(options)
